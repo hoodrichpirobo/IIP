2342f6377cc24142152ea904618bf3ea5d9c2249
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2023-10-17 12:55:30 +0200

    Update Test3.java

--- pract3/Test3.java
+++ pract3/Test3.java
@@ -10,48 +10,47 @@ import java.util.Scanner;
  *  @version Academic Year 2023-24
  */
 
- public class Test3{
 
-    public static void main(String[] args){
-        Scanner kbd=new Scanner(System.in);
+public class Test3 {
+
+    public static void main(String[] args) {
+        Scanner kbd = new Scanner(System.in);
         System.out.println("Reading hour from keyboard.");
         System.out.print("   -> Input the hour (between 0 and 23): ");
         int h = kbd.nextInt();
         System.out.print("   -> Input the minutes (between 0 and 59): ");
         int m = kbd.nextInt();
-        
-        //1
-        String hh = "" + h;
-        //2
-        String mm = "" + m;
-        //3
-        hh = "0" + h;
-        mm = "0" + mm;
-        hh = hh.substring( hh.length() - 2);
-        mm = mm.substring( mm.length() - 2);
-        
-        System.out.println("Input hour: " + hh + ":" + mm); 
-        
+
+        // Format hour and minutes as per the logic provided
+        String hh = h / 10 + "" + h % 10;
+        String mm = m / 10 + "" + m % 10;
+
+        System.out.println("Input hour: " + hh + ":" + mm);
+
         long tMinTotal = System.currentTimeMillis() / (60 * 1000);
-        
+
         int tMinCurrent = (int) (tMinTotal % (24 * 60));
-        
+
         int hC = tMinCurrent / 60;
         int mC = tMinCurrent % 60;
-        
-        String hhC = "" + hC;
-        String mmC = "" + mC;
-        hhC = "0" + hhC;
-        mmC = "0" + mmC;
-        
-        hhC = hhC.substring(hhC.length() - 2);
-        mmC = mmC.substring(mmC.length() - 2);
-        System.out.println("Current hour: " + hhC + ":" + mmC + "(UTC time)");
-        
+
+        String hhC = hC / 10 + "" + hC % 10;
+        String mmC = mC / 10 + "" + mC % 10;
+
+        System.out.println("Current hour: " + hhC + ":" + mmC + " (UTC time)");
+
         int tMinInput = h * 60 + m;
-        int dif = Math.abs (tMinInput - tMinCurrent);
-        System.out.println("Difference in minutes between the two hours: " + dif + " (" + (dif/60) + "h., " + (dif%60) + " min. )");
-     
-    }    
- 
+        int dif = Math.abs(tMinInput - tMinCurrent);
+        System.out.println("Difference in minutes between the two hours: " + dif + " (" + (dif / 60) + "h., " + (dif % 60) + " min. )");
+
+        // Check if input time is before current time
+        boolean isBefore = tMinInput < tMinCurrent;
+        System.out.println("Is previous hour " + hh + ":" + mm + " to hour "+ hhC + ":" + mmC + "? " + isBefore);
+
+        // Check if the "hh:mm" format is a palindrome
+        String timeString = hh + ":" + mm;
+        boolean isPalindrome = new StringBuilder(timeString).reverse().toString().equals(timeString);
+        System.out.println("Is palindrome the hour " + timeString + "? " + isPalindrome);
+    }
 }
+
