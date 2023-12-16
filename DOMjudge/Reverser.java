c5f69720b56c98bae3119da205e2cf0991f77448
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2023-12-16 11:37:50 +0100

    Update Reverser.java

--- DOMjudge/Reverser.java
+++ DOMjudge/Reverser.java
@@ -1,26 +1,34 @@
 import java.util.*;
 
-public class Reverser{
+public class Reverser {
+    /**
+     * Reverses a given number.
+     *
+     * @param number The number to be reversed.
+     * @return The reversed number.
+     */
     public static long reverseNumber(long number) {
-        long reversed = 0;
+        long reversed = 0; // This will store the reversed number.
 
+        // Loop until the number is reduced to 0
         while (number != 0) {
-            long lastDigit = number % 10;
-            reversed = reversed * 10 + lastDigit;
-            number /= 10;
+            long lastDigit = number % 10; // Extracts the last digit of the number
+            reversed = reversed * 10 + lastDigit; // Shifts the digits of the reversed number left and adds the last digit
+            number /= 10; // Removes the last digit from the original number
         }
 
-        return reversed;
+        return reversed; // Returns the reversed number
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in).useLocale(Locale.US);
         
+        // Loop to continuously read and reverse numbers until the input is exhausted
         while(scan.hasNext()){
-            long originalNumber = scan.nextLong();
-            long reversedNumber = reverseNumber(originalNumber);
+            long originalNumber = scan.nextLong(); // Reads the next number
+            long reversedNumber = reverseNumber(originalNumber); // Reverses the number
 
-            System.out.println(reversedNumber);            
+            System.out.println(reversedNumber); // Prints the reversed number            
         }
     }
 }
